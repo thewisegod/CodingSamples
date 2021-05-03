2b233c4518295f1f4d801c97f39e0c9b1fe8e004
thewisegod <liciandaaron@gmail.com>
2021-05-03 05:39:19 -0400

    Removed unecessary code and added Order controller.

---
+++ OrderManagementSystem_WebAPI/Controllers/Api/OrderController.cs
@@ -0,0 +1,44 @@
+﻿
+using ErrorLogger;
+using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json;
+using OrderManagementSystem_WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace OrderManagementSystem_WebAPI.Controllers.Api
+{
+    public class OrderController : ApiController
+    {
+        private readonly UnitOfWork _uow = new UnitOfWork();
+
+        [HttpGet]
+        [Route("Orders")]
+        public IEnumerable<Order> Get() => _uow.OrderRepository.Get();
+
+        [HttpGet]
+        [Route("Orders/{id}")]
+        public Order Get(int id) => _uow.OrderRepository.GetByID(id);
+
+        [HttpPost]
+        [Route("Orders/add")]
+        public IHttpActionResult Post([FromBody] Order Order) =>
+            Helper.Post<Order>(this, _uow, _uow.OrderRepository, Order, ModelState);
+
+        [HttpPut]
+        [Route("Orders/update")]
+        public IHttpActionResult Put([FromBody] Order Order) =>
+            Helper.Put<Order>(this, _uow, _uow.OrderRepository, Order, ModelState);
+
+        [HttpDelete]
+        [Route("Orders/delete/{id}")]
+        public IHttpActionResult Delete(int id) =>
+             Helper.Delete<Order>(this, _uow, _uow.OrderRepository, id, ModelState);
+    }
+}
--- OrderManagementSystem_WebAPI/UnitOfWork.cs
+++ OrderManagementSystem_WebAPI/UnitOfWork.cs
@@ -11,12 +11,14 @@ namespace OrderManagementSystem_WebAPI
     public class UnitOfWork : IDisposable
     {
         private readonly DataContext _context = new DataContext();
+        private bool _disposed = false;
         private GenericRepository<Customer> _customerRepository;
         private GenericRepository<Product> _productRepository;
-        private bool _disposed = false;
+        private GenericRepository<Order> _orderRepository;
 
         public GenericRepository<Customer> CustomerRepository => _customerRepository ?? (_customerRepository = new GenericRepository<Customer>(_context));
         public GenericRepository<Product> ProductRepository => _productRepository ?? (_productRepository = new GenericRepository<Product>(_context));
+        public GenericRepository<Order> OrderRepository => _orderRepository ?? (_orderRepository = new GenericRepository<Order>(_context));
 
         public void Complete()
         {
